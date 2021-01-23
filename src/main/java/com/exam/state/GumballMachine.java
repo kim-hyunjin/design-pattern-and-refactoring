0a257bd2eefb067f0d35a6e80b36f0e5a359f9c0
Kim <hyunjin1612@gmail.com>
2021-01-23 21:45:23 +0900

    스테이트 패턴 적용 전

---
+++ src/main/java/com/exam/state/GumballMachine.java
@@ -0,0 +1,93 @@
+package com.exam.state;
+
+public class GumballMachine {
+
+    final static int SOLD_OUT = 0;
+    final static int NO_QUARTER = 1;
+    final static int HAS_QUARTER = 2;
+    final static int SOLD = 3;
+
+    int state = SOLD_OUT;
+    int count = 0;
+
+    public GumballMachine(int count) {
+        this.count = count;
+        if (count > 0) {
+            state = NO_QUARTER;
+        }
+    }
+
+    public void insertQuarter() {
+        if (state == HAS_QUARTER) {
+            System.out.println("동전은 한 개만 넣어주세요.");
+        } else if (state == NO_QUARTER) {
+            state = HAS_QUARTER;
+            System.out.println("동전을 넣으셨습니다.");
+        } else if (state == SOLD_OUT) {
+            System.out.println("매진되었습니다. 다음 기회에 이용해주세요.");
+        } else if (state == SOLD) {
+            System.out.println("잠깐만 기다려주세요. 알맹이가 나가고 있습니다.");
+        }
+    }
+
+    public void ejectQuarter() {
+        if (state == HAS_QUARTER) {
+            System.out.println("동전이 반환됩니다");
+            state = NO_QUARTER;
+        } else if (state == NO_QUARTER) {
+            System.out.println("반환할 동전이 없습니다.");
+        } else if (state == SOLD_OUT) {
+            System.out.println("매진되었습니다. 반환할 동전이 없습니다.");
+        } else if (state == SOLD) {
+            System.out.println("이미 알맹이를 뽑으셨습니다.");
+        }
+    }
+
+    public void turnCrank() {
+        if (state == HAS_QUARTER) {
+            System.out.println("손잡이를 돌리셨습니다.");
+            state = SOLD;
+            dispense();
+        } else if (state == NO_QUARTER) {
+            System.out.println("동전을 넣어주세요.");
+        } else if (state == SOLD_OUT) {
+            System.out.println("매진되었습니다.");
+        } else if (state == SOLD) {
+            System.out.println("손잡이는 한 번만 돌려주세요.");
+        }
+    }
+
+    private void dispense() {
+        if (state == SOLD) {
+            System.out.println("알맹이가 나가고 있습니다.");
+            count--;
+            if (count == 0) {
+                System.out.println("더 이상 알맹이가 없습니다.");
+                state = SOLD_OUT;
+            } else {
+                state = NO_QUARTER;
+            }
+        } else if (state == NO_QUARTER) {
+            System.out.println("동전을 넣어주세요.");
+        } else if (state == SOLD_OUT) {
+            System.out.println("매진입니다.");
+        } else if (state == HAS_QUARTER) {
+            System.out.println("알맹이가 나갈 수 없습니다.");
+        }
+    }
+
+    public String toString() {
+        String value = "\n주식회사 왕뽑기\n자바로 돌아가는 2004년형 뽑기 기계\n남은 개수: "
+                + this.count + "개";
+        if (this.state == 0) {
+            value += "\n매진";
+        } else if (this.state == 1) {
+            value += "\n동전 투입 대기중";
+        } else if (this.state == 2) {
+            value += "\n동전 투입됨";
+        } else if (this.state == 3) {
+            value += "\n알맹이가 나갔습니다";
+        }
+        return value += "\n";
+    }
+}
